9a6126af7a140a8834ff7f6084aae5ed93e541f1
Freddy Persson <FreddyPersson85@gmail.com>
2017-12-19 15:18:28 +0100

    Added a functionall phantom-scraper

--- app.js
+++ app.js
@@ -1,73 +1,52 @@
 const phantom = require('phantom');
 const asleep = require('asleep');
 
-const screenshots = __dirname + '/phantom-screenshots/';
+runScraper();
 
 async function runScraper(){
-	const instance = await phantom.create();
 
+	const instance = await phantom.create();
 	const page = await instance.createPage();
 
-	async function waitForPageLoad() {
-		while(! await page.evaluate(function() {
-
-			if (!window.$) {
-				return false;
-			}
-
-			$(function() {
-				window.domLoaded = true;
-			});
-
-			return window.domLoaded;
-		})) {
-			await asleep(100);
-		}
-	}
-
-	await page.property('viewportSize', {width: 1024, height: 2000});
-
-  	await page.open('http://www.dmi.dk/vejr/til-lands/byvejr/by/vis/SE/2662149/Ystad,%20Sverige').catch((err)=>console.log(err))
+  	await page.open('http://www.dmi.dk/vejr/til-lands/byvejr/by/vis/SE/2662149/Ystad,%20Sverige').catch((err)=>console.log(err));
 
   	let content = await page.property('content');
+  	let weatherTd = [];
 
-  	page.evaluate(function(){
-		$('#ui-id-3').click();
-  	});
-
-  	screenshots && page.render(screenshots + 'weather.jpg');
-
-  	await waitForPageLoad();
-
-  	let searchResult = await page.evaluate(function(){
-  		var weatherInformation = [];
-		var informationToPush = {};
-		var columnText = ['time', 'degrees', 'rain', 'wind'];
-
-		var weatherTD = await page(($) => {
-			return $('#hour_table table').first().find('tbody td').map(function() {
-				return $(this).text();
+  	while (!weatherTd.length){
+	  	weatherTd = await page.evaluate(function() {
+			var arr = [];
+			$('#hour_table table').first().find('tbody td').each(function() {
+				arr.push($(this).text());
 			});
+			return arr;
 		});
-		console.log('weatherTD', weatherTD);
+		await asleep(100);
+	}
 
-		for (var i = 0; i < weatherTD.length; i++) {
+	let searchResult = await constructingColumns(weatherTd);
 
-			var column = i % 4;
+  	async function constructingColumns(data) {
+		var weatherInformation = [];
+		var columnKey = ['time', 'DanishLoremIpsum', 'degrees', 'rain', 'wind'];
 
-			weatherInformation[0] = columnText[column];
+		for (var i = 0; i < data.length; i += 5) {
 
-			if (i === 0 && column === 0) {
-				weatherInformation.push(informationToPush);
-				informationToPush = {};
-			}
+			let informationToPush = {};
+
+			columnKey.forEach(function (key, index) {
+				if (index !== 1) {
+					informationToPush[key] = data[i + index];
+				}
+			});
+
+			weatherInformation.push(informationToPush);
 		}
+
 		return weatherInformation;
-	});
+	}
 
 	console.log(searchResult);
 
 	console.log("DONE");
 }
-
-runScraper();
